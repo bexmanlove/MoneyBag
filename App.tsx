9433a26af60da06b288211d97fe507f9422641f1
Manlove, Sarah <sarah.manlove@optum.com>
2022-09-06 10:57:33 -0400

    add preliminary files

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+//custom font
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
+import Welcome from './screens/Welcome';
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
+    "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+   <Welcome />
+  );
+}
+
